ef9475c01c7a8d2ae7ef4a0a801611b77f60a131
davebha <davebharat93@gmail.com>
2026-07-31 18:51:36 +0000

    Reading a file contents through file object created in Ch6/readfile_start.py

--- Start/Ch6/readfile_start.py
+++ Start/Ch6/readfile_start.py
@@ -3,7 +3,13 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-    
-# Open the file and read the contents
 
+# Open the file and read the contents
+sample_file = open("textfile.txt","r")
+if sample_file.mode == "r":
     # use the read() function to read the entire file
+    #contents = sample_file.read()
+    #print(contents)
+    file_lines = sample_file.readlines()
+    for line in file_lines:
+        print(line)
